c5846786df922dd2d81ee608a18850934fcdf29c
andrew <andrew.key@redpanda.ai>
2019-01-23 14:36:43 -0800

    Adding meetup-api library to requirements file. Creating a meetup_api demo module.

--- meetup_api_demo.py
+++ meetup_api_demo.py
@@ -1,21 +1,11 @@
+import math
 import meetup.api
 import os
-import logging
-import math
 
 PAGE_SIZE = 200
-logging.getLogger("meetup.api").setLevel(logging.WARNING)
 
-if 'MEETUP_API_KEY' not in os.environ:
-    raise AssertionError('MEETUP_API_KEY not found in environment, aborting execution.')
-
-API_KEY = os.environ['MEETUP_API_KEY']
-print(f"The API_KEY in the environment is {API_KEY}")
-
-client = meetup.api.Client(API_KEY)
-
-
-def print_members_starting_with_page(page_number, step=1):
+def print_members_starting_with_page(client, page_number, step=1):
+    """Loops through pages until there are no more"""
     print(f"Offset: {page_number}, Step: {step}")
     page_number = page_number
     keep_going = True
@@ -40,22 +30,31 @@ def print_members_starting_with_page(page_number, step=1):
 
 
 if __name__ == "__main__":
-    # This is the Sunnyvale AI Frontiers forum at https://www.meetup.com/svaibd/
-    url = 'svaibd'
+    """Main program"""
+
+    # Get the API key from the operating system's environment
+    if 'MEETUP_API_KEY' not in os.environ:
+        raise AssertionError('MEETUP_API_KEY not found in environment, aborting execution.')
+
+    API_KEY = os.environ['MEETUP_API_KEY']
+    print(f"The API_KEY in the environment is {API_KEY}")
+
+    client = meetup.api.Client(API_KEY)
+
+    url = 'svaibd'  # This is the Sunnyvale AI Frontiers forum at https://www.meetup.com/svaibd/
     group_info = client.GetGroup({'urlname': 'svaibd'})
     keys = list(group_info.__dict__.keys())
 
     # Display group information
-    #for key in keys:
-    #    print(f"Key: {key}, Value: {group_info.__dict__[key]}")
+    for key in keys:
+        print(f"Key: {key}, Value: {group_info.__dict__[key]}")
 
     total_pages = math.ceil(group_info.members / float(PAGE_SIZE))
     print(f"There are {group_info.members} members in this group and {total_pages} total pages.")
     print(total_pages)
 
-    #NOTE: This is a candidate for asynchronous i/o instead of a single synchronous loop
+    # NOTE: This is a candidate for asynchronous i/o instead of a single synchronous loop
 
-    # print the last page
-    print_members_starting_with_page(total_pages - 1)
-
+    # print the last pages
+    print_members_starting_with_page(client, total_pages - 1)
 
